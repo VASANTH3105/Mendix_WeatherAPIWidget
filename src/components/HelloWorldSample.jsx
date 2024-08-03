6338411e57981c004d6c2c98fe78ca7baf7a7fbd
VASANTH_3105 <vasanth.it2125@gmail.com>
2024-08-04 01:40:31 +0530

    Weather API Widget

--- src/components/HelloWorldSample.jsx
+++ src/components/HelloWorldSample.jsx
@@ -1,5 +1,42 @@
-import { createElement } from "react";
+import React, { createElement, useState } from "react";
+
+const api = {
+    key: "69b0c482eebc89a44a87223bf24dbf82",
+    base: "https://api.openweathermap.org/data/2.5/"
+};
 
 export function HelloWorldSample({ sampleText }) {
-    return <div className="widget-hello-world">Hello {sampleText}</div>;
+    const [search, setSearch] = useState("");
+    const [weather, setWeather] = useState({});
+
+    const searchPressed = () => {
+        fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+            .then((res) => res.json())
+            .then((result) => {
+                setWeather(result);
+            });
+    }
+
+    return (
+        <>
+            <div className="widget-hello-world">Weather widget welcomes you, {sampleText}</div>
+            <div>
+                <input type="text" placeholder="Search here..." onChange={e => setSearch(e.target.value)} />
+                <button onClick={searchPressed}>Search</button>
+            </div>
+
+            {weather.name && (
+                <div>
+                    <p>Location: {weather.name}</p>
+                    {weather.main && <p>Temperature: {weather.main.temp} °C</p>}
+                    {weather.weather && weather.weather.length > 0 && (
+                        <div>
+                            <p>Weather: {weather.weather[0].main}</p>
+                            <p>Description: {weather.weather[0].description}</p>
+                        </div>
+                    )}
+                </div>
+            )}
+        </>
+    );
 }
---
+++ src/components/WeatherAPI.jsx
@@ -0,0 +1,11 @@
+
+
+export default WeatherAPI
+
+export function WeatherAPI() {
+    return (
+        <>
+        <h1>Hello</h1>
+        </>
+    );
+}
